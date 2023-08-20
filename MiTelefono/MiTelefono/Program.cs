51caf4d8887587f052936e76c2ec5ba8d2b4d4ec
MarcoTeo0 <142039407+MarcoTeo0@users.noreply.github.com>
2023-08-20 17:26:53 -0600

    telefono

---
+++ MiTelefono/MiTelefono/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace MiTelefono
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SmartPhone RedmiK40Gaming = new SmartPhone();
+            RedmiK40Gaming.networkTech = LTE;
+            RedmiK40Gaming.Launch = 2021;
+            RedmiK40Gaming.DayLaunch = 27;
+            RedmiK40Gaming.DimensionsAlto = 161.9;
+            RedmiK40Gaming.DimensionsAncho = 76.9;
+            RedmiK40Gaming.Weight = 205;
+            RedmiK40Gaming.Build = GlassFront;
+            RedmiK40Gaming.SIM = true;
+            RedmiK40Gaming.Type = OLED;
+            RedmiK40Gaming.Brillo = 500;
+            RedmiK40Gaming.Size = 6.67;
+            RedmiK40Gaming.ResolutionAncho = 1080;
+            RedmiK40Gaming.ResolutionAlto = 2400;
+            RedmiK40Gaming.Protection = true;
+            RedmiK40Gaming.NameProtection = CorllingGorillaGlass5;
+        }
+    }
+}
---
+++ MiTelefono/MiTelefono/SmartPhone.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiTelefono
+{
+    public class SmartPhone
+    {
+        public string networkTech;
+        public int Launch;
+        public int DayLaunch;
+        //body
+        public float DimensionsAlto;
+        public float DimensionsAncho;
+        public int Weight;
+        public string Build;
+        public bool SIM;
+        //display
+        public string Type;
+        public int Brillo;
+        public float Size;
+        public int ResolutionAncho;
+        public int ResolutionAlto;
+        public bool Protection;
+        public string NameProtection;
+        //plataform
+        public int OSAndroid;
+            public float OSPersonalized;
+        public string Chipset;
+        public string CPU;
+        public string GPU;
+        //Memory
+        public bool SlotSD;
+        public int MemoryInt;
+        public int RAM;
+        public float typeMemory;
+        //Camera
+        public char NoCameras;
+        public int MPXCamera1;
+        public string TypeCamera;
+        public float focalCamera1;
+        public int SizeCamera1;
+        public int MPXCamera2;
+        public float focalCamera2;
+        public int SizeCamera2;
+        public int MPXCamera3;
+        public float focalCamera3;
+        public bool Flash;
+        //video
+        public string ResolutionVideo;
+        public int FPSvideo;
+        //Sound
+        public bool Soundspeaker;
+        public int NoAltavoces;
+        public bool Jack;
+        //COOMS
+        public string WLAN;
+        public float Bluetooth;
+        public string Positioning;
+        public bool NFC;
+        public bool Infraredport;
+        public bool radio;
+        public string USBType;
+        public string Sensors;
+        public int mAhBattery;
+        public int ChargeSupport;
+        public string Color;
+        public string Model;
+        public int PriceinDollars;
+
+
+
+
+    }
+}
