730cfde5d078a79114352074a7226680aff87edf
MarcoTeo0 <142039407+MarcoTeo0@users.noreply.github.com>
2023-08-27 15:48:35 -0600

    xd
    xd

--- ClasesIN/CarreraCoches/Program.cs
+++ ClasesIN/CarreraCoches/Program.cs
@@ -4,18 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Coche ferrari = new Coche();
+            Auto ferrari = new Auto();
             ferrari.modelo = "Ferrari F1";
             ferrari.velodidad = 120;
 
-            Coche mcLaren = new Coche();
-            mcLaren.modelo = "MC 2443"
+            Auto mcLaren = new Auto();
+            mcLaren.modelo = "MC 2443";
             mcLaren.velodidad = 140;
 
             PistaCarreras pista1 = new PistaCarreras();
-            pista1.nombre = "Hermanos Rodriguez"
+            pista1.nombre = "Hermanos Rodriguez";
                 pista1.longitud = 4.303f;
-            pista1.Competir(ferrir, mcLaren);
+
+
+
+            ferrari.PonerGasolina();
+            ferrari.Arrancar();
+            mcLaren.Arrancar();
+
+
+
+            pista1.Competir(ferrari, mcLaren);
             //presinar Enter para continuar
             Console.ReadLine();
 
