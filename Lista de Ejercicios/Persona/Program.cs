efadf8b52bd13bef2a279a351c43b3dc8aa4470a
MarcoTeo0 <142039407+MarcoTeo0@users.noreply.github.com>
2023-08-31 22:26:19 -0600

    buh

---
+++ Lista de Ejercicios/Persona/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Persona
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona("Pedro, 18, Masculino");
+
+
+            Console.WriteLine("Nombre: " + persona1.GetNombre());
+            Console.WriteLine("Edad: " + persona1.GetEdad());
+            Console.WriteLine("Género: " + persona1.GetGenero());
+
+            // Establecer nuevos valores de atributos
+            persona1.SetNombre("Ana");
+            persona1.SetEdad(25);
+            persona1.SetGenero("Femenino");
+
+            // Mostrar los nuevos valores de atributos
+            Console.WriteLine("Nombre: " + persona1.GetNombre());
+            Console.WriteLine("Edad: " + persona1.GetEdad());
+            Console.WriteLine("Género: " + persona1.GetGenero());
+        }
+    }
+}
